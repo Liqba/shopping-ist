efedc9c26451d035886a486fee79bfd1a1d1f565
Muhammad Iqbal <miqbalsparta2004@gmail.com>
2023-11-22 14:25:47 +0700

    deletion csrf_extempt

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -146,3 +146,6 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+
+
+CSRF_TRUSTED_ORIGINS = ['https://muhammad-iqbal215-tutorial.pbp.cs.ui.ac.id']
